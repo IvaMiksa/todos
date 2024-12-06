67f36b6991346330efecec501826348fb039d000
iva.miksa <iva.miksa@gmail.com>
2024-12-06 18:58:55 +0100

    adding toast notifications

--- todos/src/main.jsx
+++ todos/src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './store/slices/store'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <ToastContainer />
     <App />
-  </Provider>,
-)
+  </Provider>
+);
