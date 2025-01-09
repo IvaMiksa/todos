892081dd07c1d5571df34182cdf21909d0bc5072
iva.miksa <iva.miksa@gmail.com>
2025-01-09 14:43:40 +0100

    delete a completed/uncompleted todo tests added

--- todos/tests/todos.spec.ts
+++ todos/tests/todos.spec.ts
@@ -37,10 +37,55 @@ test.describe("Todos App", () => {
     const completedTodo = page.locator(".line-through");
     await expect(completedTodo).toBeVisible();
     await expect(checkbox).toBeChecked();
+
+    // Unmark the todo
+    await checkbox.click(); // uncheck??
+
+    // Assert the todo is no longer marked as completed
+    await expect(completedTodo).not.toBeVisible();
+    await expect(checkbox).not.toBeChecked();
   });
 
-  //test('unmark todo as completed')
-  //test('delete a todo')
+  test("delete a completed todo", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    // Add a new todo
+    const inputField = page.locator('input[placeholder="Your next todo"]');
+    await inputField.fill("Take a walk");
+    const buttonAdd = page.locator('button:text-is("Add")');
+    await buttonAdd.click();
+
+    // Mark todo as completed
+    const checkbox = page.locator('input[type="checkbox"]');
+    await checkbox.click(); // vs check() ??
+
+    // Delete completed todo
+    const deleteIcon = page.locator("#delete-icon");
+    await deleteIcon.click();
+
+    // Assert the deleted todo is no longer visible
+    const deletedTodo = page.locator(':text-is("Take a walk")');
+    await expect(deletedTodo).not.toBeVisible();
+  });
+
+  test("delete an uncompleted todo", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    // Add a new todo
+    const inputField = page.locator('input[placeholder="Your next todo"]');
+    await inputField.fill("Take a walk");
+    const buttonAdd = page.locator('button:text-is("Add")');
+    await buttonAdd.click();
+
+    // Delete uncompleted todo
+    const deleteIcon = page.locator("#delete-icon");
+    await deleteIcon.click();
+
+    // Assert the deleted todo is no longer visible
+    const deletedTodo = page.locator(':text-is("Take a walk")');
+    await expect(deletedTodo).not.toBeVisible();
+  });
+
   //test('edit a todo')
   //test('search todos')
   //test('filter by status')
