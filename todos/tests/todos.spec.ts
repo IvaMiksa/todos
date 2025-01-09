36fe2e2f3660b9c16821fe2d6a6d63181a936ed4
iva.miksa <iva.miksa@gmail.com>
2025-01-09 19:05:12 +0100

    pagination tests added

--- todos/tests/todos.spec.ts
+++ todos/tests/todos.spec.ts
@@ -269,8 +269,31 @@ test.describe("Todos App", () => {
     await expect(uncompletedTodo).toBeVisible();
   });
 
-  
-  //test('next page')
-  //test('previous page')
+  test("pagination", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    // Generate 6 todos to get 2 pages
+    for (let i = 1; i < 7; i++) {
+      const inputField = page.locator('input[placeholder="Your next todo"]');
+      await inputField.fill(`Todo #${i}`);
+      const buttonAdd = page.locator('button:text-is("Add")');
+      await buttonAdd.click();
+    }
+
+    // Next page
+    const nextPageButton = page.locator('button:text-is("Next")');
+    await nextPageButton.click();
+
+    const todoOnSecondPage = page.locator(':text-is("Todo #6")');
+    await expect(todoOnSecondPage).toBeVisible();
+
+    // Previous page
+    const previousPageButton = page.locator('button:text-is("Previous")');
+    await previousPageButton.click();
+
+    const firstPageTodosCount = await page.locator("li").allTextContents();
+    await expect(firstPageTodosCount.length).toBe(5);
+  });
+
   //test('persist new todo in local storage')
 });
